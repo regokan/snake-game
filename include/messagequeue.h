01d474bb387e13bf20e507e8515184cd8a7e5e24
regokan <regokan@gmail.com>
2024-10-13 05:08:30 +0530

    feat: Add MessageQueue template class for thread-safe messaging

---
+++ include/messagequeue.h
@@ -0,0 +1,40 @@
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <optional>
+
+template <typename T>
+class MessageQueue {
+ public:
+  void send(T &&msg) {
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
+  }
+
+  T receive() {
+    std::unique_lock<std::mutex> lock(_mutex);
+    _cond.wait(lock, [this] { return !_queue.empty(); });
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
+  }
+
+  std::optional<T> try_receive() {
+    std::lock_guard<std::mutex> lock(_mutex);
+    if (_queue.empty()) return std::nullopt;
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
+  }
+
+  std::size_t size() {
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _queue.size();
+  }
+
+ private:
+  std::deque<T> _queue;
+  std::mutex _mutex;
+  std::condition_variable _cond;
+};

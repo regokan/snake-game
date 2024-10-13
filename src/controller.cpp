8cbb6132ea84b808c7c4ecafaa617b23e6271a0d
regokan <regokan@gmail.com>
2024-10-13 15:34:29 +0530

    refactor: adjust snake speed logic to use percentage-based increments for smoother acceleration

--- src/controller.cpp
+++ src/controller.cpp
@@ -11,8 +11,8 @@ void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
 
 void Controller::HandleInput(bool &running, Snake &snake) const {
   SDL_Event e;
-  const float speed_increment = 0.01f;
-  const float max_speed = 0.3f;
+  const float speed_increment_percent = 0.25f;
+  const float max_speed = 0.5f;
 
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
@@ -22,7 +22,7 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
         case SDLK_UP:
         case SDLK_w:
           if (snake.direction == Snake::Direction::kUp) {
-            snake.speed = std::min(snake.speed + speed_increment, max_speed);
+            snake.speed = std::min(snake.speed * (1 + speed_increment_percent), max_speed);
           } else {
             ChangeDirection(snake, Snake::Direction::kUp, Snake::Direction::kDown);
           }
@@ -31,7 +31,7 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
         case SDLK_DOWN:
         case SDLK_s:
           if (snake.direction == Snake::Direction::kDown) {
-            snake.speed = std::min(snake.speed + speed_increment, max_speed);
+            snake.speed = std::min(snake.speed * (1 + speed_increment_percent), max_speed);
           } else {
             ChangeDirection(snake, Snake::Direction::kDown, Snake::Direction::kUp);
           }
@@ -40,7 +40,7 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
         case SDLK_LEFT:
         case SDLK_a:
           if (snake.direction == Snake::Direction::kLeft) {
-            snake.speed = std::min(snake.speed + speed_increment, max_speed);
+            snake.speed = std::min(snake.speed * (1 + speed_increment_percent), max_speed);
           } else {
             ChangeDirection(snake, Snake::Direction::kLeft, Snake::Direction::kRight);
           }
@@ -49,7 +49,7 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
         case SDLK_RIGHT:
         case SDLK_d:
           if (snake.direction == Snake::Direction::kRight) {
-            snake.speed = std::min(snake.speed + speed_increment, max_speed);
+            snake.speed = std::min(snake.speed * (1 + speed_increment_percent), max_speed);
           } else {
             ChangeDirection(snake, Snake::Direction::kRight, Snake::Direction::kLeft);
           }
